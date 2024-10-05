f5e90209d93b990bc8f6d99f6377175eb9652cd1
Jaxeetoo <jacobesclamado2@gmail.com>
2024-10-05 21:59:11 +1000

    [pages] created an error page and setted up react route dom

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      <div className='text-white flex flex-col gap-10'>
+        <h1 className='text-4xl font-bold text-center'>OOPS!</h1>
+        <h3 >Error 404 or something like that...</h3>
+      </div>
+    </div>
+    
+  )
+}
+
+export default ErrorPage
